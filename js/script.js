bde2bd3f945adbc80dac3761bcb13f828530fd7e
Dawid Galeski <dawidgaski@gmail.com>
2023-03-15 21:58:36 +0100

    added remove task

--- js/script.js
+++ js/script.js
@@ -10,20 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-            ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTasks = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -31,6 +17,35 @@
 
         render();
     }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+            ${task.content}
+            <button class= "js-remove">usuń</button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
